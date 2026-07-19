584b8568d5dc024ae0e633c099771b1b2647a34f
hadryphern <219745641+hadryphern@users.noreply.github.com>
2026-07-19 21:04:41 +0100

    Reset Fabric 1.20.1 to a clean foundation

--- Fabric/1.20.1/src/main/java/net/blue/chaoticd/ChaoticDimensions.java
+++ Fabric/1.20.1/src/main/java/net/blue/chaoticd/ChaoticDimensions.java
@@ -1,30 +1,16 @@
 package net.blue.chaoticd;
 
 import net.fabricmc.api.ModInitializer;
-import net.blue.chaoticd.content.block.ModBlocks;
-import net.blue.chaoticd.content.block.ModBlockEntities;
-import net.blue.chaoticd.content.block.RosalitaAxeStripping;
-import net.blue.chaoticd.content.block.RosalitaVanillaIntegrations;
-import net.blue.chaoticd.content.item.ModItems;
-import net.blue.chaoticd.content.tab.ModCreativeTabs;
-import net.blue.chaoticd.worldgen.ModWorldgen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/** Main Fabric entrypoint for Chaotic Dimensions. */
+/** Empty Fabric foundation for the next Chaotic Dimensions implementation. */
 public final class ChaoticDimensions implements ModInitializer {
     public static final String MOD_ID = "chaoticd";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        ModBlocks.initialize();
-        ModBlockEntities.initialize();
-        RosalitaAxeStripping.initialize();
-        ModItems.initialize();
-        RosalitaVanillaIntegrations.initialize();
-        ModCreativeTabs.initialize();
-        ModWorldgen.initialize();
-        LOGGER.info("Initializing Chaotic Dimensions on Fabric");
+        LOGGER.info("Chaotic Dimensions clean foundation loaded");
     }
 }
